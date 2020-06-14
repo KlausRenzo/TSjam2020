ccea71c1a848d7eaed4747ccd61f2475e517cbb9
DrDust0 <salvatoresolimeno70@gmail.com>
2020-06-14 12:54:09 +0200

    Added animations, fixed navmesh bug

--- TheSIGN jam 2020/Assets/Scripts/CharacterLocomotion.cs
+++ TheSIGN jam 2020/Assets/Scripts/CharacterLocomotion.cs
@@ -6,40 +6,46 @@ using System.Linq;
 public class CharacterLocomotion : MonoBehaviour
 {
     public Character character;
-    public List<Vector3> path = new List<Vector3>();
+    public List<Vector3> pathCoordinates = new List<Vector3>();
     private NavMeshAgent agent;
     [SerializeField]private float maxDistance = 0.05f;
+    private NavMeshPath navPath;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        navPath = new NavMeshPath();
         //agent.speed = character.stats.speed;
     }
 
     public void Update()
     {
 
-        if (path.Count > 0)
+        if (pathCoordinates.Count > 0)
         {
-            var dir = (path[0] - transform.position).normalized;
-            var distanceFromCurrentPoint = Vector3.Distance(transform.position, path[0]);
+            var dir = (pathCoordinates[0] - transform.position).normalized;
+            var distanceFromCurrentPoint = Vector3.Distance(transform.position, pathCoordinates[0]);
             var newPos = transform.position + dir * Time.deltaTime * character.stats.speed;
             var distanceFromNewPos = Vector3.Distance(transform.position, newPos);
             transform.position += dir * (((distanceFromNewPos > distanceFromCurrentPoint)) ? distanceFromCurrentPoint : distanceFromNewPos);
             transform.LookAt(transform.position + dir);
-            if (Vector3.Distance(transform.position, path[0]) < maxDistance)
+            character.animationHandler.Play(character.animationHandler.movementAnimation);
+            if (Vector3.Distance(transform.position, pathCoordinates[0]) < maxDistance)
             {
-                path.RemoveAt(0);
+                pathCoordinates.RemoveAt(0);
+                if(pathCoordinates.Count == 0)
+                {
+                    character.animationHandler.Play(character.animationHandler.idleAnimation);
+                }
             }
         }
+        
     }
 
     public void ChangeTarget(Vector3 target)
     {
-        agent.SetDestination(target);
-        path = agent.path.corners.ToList();
-        foreach(var x in path)
-        {
-            Debug.Log(x);
-        }
+        agent.ResetPath();
+        NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, navPath);
+        agent.SetPath(navPath);
+        pathCoordinates = agent.path.corners.ToList();
     }
 }
--- TheSIGN jam 2020/Assets/Scripts/GameManager.cs
+++ TheSIGN jam 2020/Assets/Scripts/GameManager.cs
@@ -11,9 +11,11 @@ public class GameManager : MonoBehaviour
     public List<Character> characters = new List<Character>();
     public Transform spawnPoint;
     private int currentCharacter = -1;
+    private List<Collider> charactersColliders = new List<Collider>();
 
     public void Start()
     {
+        SetCollisionAvoidance();
         SetNextCharacter();
     }
 
@@ -25,9 +27,9 @@ public class GameManager : MonoBehaviour
             OnCharacterDeath -= characters[currentCharacter].Die;
         }
         currentCharacter++;
-        if(currentCharacter > characters.Count)
+        if(currentCharacter > characters.Count-1)
         {
-            //Die!
+            return;
         }
         for(int i = 0; i <= currentCharacter; i++)
         {
@@ -41,5 +43,25 @@ public class GameManager : MonoBehaviour
             characters[i].entity.GOJHONNYGO();
         }
         OnCharacterDeath += characters[currentCharacter].Die;
+    }
+    public void SetCollisionAvoidance()
+    {
+        foreach(var c in characters)
+        {
+            c.gameObject.SetActive(true);
+            charactersColliders.Add(c.GetComponent<Collider>());
+        }
+        for(int i = 0; i < charactersColliders.Count-1; i++)
+        {
+            for (int j = i + 1; j < charactersColliders.Count; j++)
+            {
+                Physics.IgnoreCollision(charactersColliders[i], charactersColliders[j],true);
+            }
+           
+        }
+        foreach(var c in characters)
+        {
+            c.gameObject.SetActive(false);
+        }
     }
 }

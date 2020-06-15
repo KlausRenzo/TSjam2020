fa6bae5dea98aedda87964b81adff77069503b6c
Domiziano <domizianoatticciati@gmail.com>
2020-06-15 04:28:38 +0200

    Added soundtrack

---
+++ TheSIGN jam 2020/Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Assets.Scripts.Player;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Assets.Scripts.Managers
+{
+	public class SoundManager : Manager
+	{
+		[SerializeField] private Sound soundtrack;
+		
+		
+
+		protected override void OnManagerDestroy()
+		{
+		}
+
+		protected override void OnManagerAwake()
+		{
+			var source = gameObject.AddComponent<AudioSource>();
+			source.loop = true;
+			soundtrack.Play(source);
+		}
+
+
+	}
+}
